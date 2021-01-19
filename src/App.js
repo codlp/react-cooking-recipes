df0c05b3b40fbd7c5c23cc3f3e79ea984e76924b
coralie delpha <coralie.delpha@gmail.com>
2021-01-19 11:46:52 +0100

    create App component and protect API keys

--- src/App.js
+++ src/App.js
@@ -1,2 +1,21 @@
 import React from "react";
 import './App.css';
+
+function App() {
+
+  const exampleApiRequest = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
+
+  return (
+    <div className="App">
+      <h1>Cooking Recipes</h1>
+      <form className="search-form" action="">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
