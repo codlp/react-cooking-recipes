83104c3f7e7699a54f569b0cba7001f40c4245ab
coralie delpha <coralie.delpha@gmail.com>
2021-01-19 12:37:36 +0100

    first API call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
 
-  const exampleApiRequest = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
+  // This is not a secure way to store API keys in a React app
+  // it is just for simple exercise purposes
+  const appId = process.env.REACT_APP_APP_ID;
+  const appKey = process.env.REACT_APP_APP_KEY;
 
+  const exampleApiRequest = `/search?q=chicken&app_id=${appId}&app_key=${appKey}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(exampleApiRequest);
+    const data = await response.json();
+    console.log(data);
+  };
+  
   return (
     <div className="App">
       <h1>Cooking Recipes</h1>
@@ -16,6 +31,6 @@ function App() {
       </form>
     </div>
   );
-}
+};
 
 export default App;
